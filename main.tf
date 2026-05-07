a53b0247ec99a6e54e5d6a55f4c725f56378bf19
JyotiTeli6 <jyotit@nousinfo.com>
2026-05-07 16:33:40 +0530

    Add initial Terraform configuration for Azure
    Initialize Terraform configuration with Azure provider and resources.

---
+++ main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "drift-demo-rg"
+  location = "Central India"
+}
+
+resource "azurerm_storage_account" "storage" {
+  name                     = "driftdemostg12345"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
